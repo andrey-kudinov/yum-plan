eee9f406c859e43b69274b61589bf97fe5417eaa
Aleksandr <87819638+aleksandr24789@users.noreply.github.com>
2024-03-22 10:57:53 +0000

    Update styles

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -5,12 +5,27 @@ import styles from './RegistrationForm.module.css'
 export default function RegistrationForm() {
   return (
     <form className={styles['registration-form']}>
-      <label>Ваше имя</label>
-      <FormInput inputType='text'/>
-      <label>E-mail</label>
-      <FormInput inputType='text'/>
-      <label>Пароль</label>
-      <FormInput inputType='password'/>
+      <label htmlFor="user-name">
+        Ваше имя
+        <span className={styles['text-error']}>
+          Это поле обязательно
+        </span>
+      </label>
+      <FormInput inputType='text' name="user-name"/>
+      <label htmlFor="user-email">
+        E-mail
+        <span className={styles['text-error']}>
+          Это поле обязательно
+        </span>
+      </label>
+      <FormInput inputType='text' name="user-email"/>
+      <label htmlFor="user-password">
+        Пароль
+        <span className={styles['text-error']}>
+          Это поле обязательно
+        </span>
+      </label>
+      <FormInput inputType='password' name="user-password"/>
       <FormButton>Зарегистрироваться</FormButton>
     </form>
   )
