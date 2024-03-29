e6d651bf83432f7cd15a0e14316fa27933cccebb
Aleksandr <87819638+aleksandr24789@users.noreply.github.com>
2024-03-29 10:19:31 +0000

    Update Singin page

--- src/layouts/RegLayout/RegLayout.js
+++
@@ -1,9 +0,0 @@
-import styles from './RegLayout.module.css'
-
-export default function RegLayout({ children }) {
-  return (
-    <main className={styles['main-registration']}>
-      {children}
-    </main>
-  )
-}
---
+++ src/pages/signin/index.js
@@ -0,0 +1,38 @@
+import { getProviders, signIn } from "next-auth/react"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]"
+import SigninLayout from '@/layouts/SigninLayout/SigninLayout'
+import styles from './signin.module.css'
+
+export default function Signin({ providers }) {
+  return (
+    <SigninLayout>
+      <div className={styles['signin']}>
+        <div className={styles['signin-block']}>
+          <h1 className={styles['signin-title']}>Войти с помощью:</h1>
+            {Object.values(providers).map((provider) => (
+              <div key={provider.name}>
+                <button onClick={() => signIn(provider.id)}>
+                  Sign in with {provider.name}
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
+    </SigninLayout>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getServerSession(context.req, context.res, authOptions)
+
+  if (session) {
+    return { redirect: { destination: "/" } }
+  }
+  
+  const providers = await getProviders()
+
+  return {
+    props: { providers: providers ?? [] }
+  }
+}
