944d3993109a7f4485d9cbd52821dc4c3dde0e5e
Aleksandr <87819638+aleksandr24789@users.noreply.github.com>
2024-04-28 16:07:03 +0500

    Add form verification

--- src/components/SetUserForm/SetUserForm.js
+++ src/components/SetUserForm/SetUserForm.js
@@ -1,23 +1,32 @@
-'use client'
-
 import { useState } from 'react'
 import { useStateContext } from '@/providers/stateContext'
+import { z } from 'zod'
 import styles from './SetUserForm.module.css'
 
 export default function SetUserForm() {
   const [buttonLoading, setButtonLoading] = useState(false)
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState(null)
   const { user, setUser } = useStateContext()
   if (!user) return null
   
   const onSubmit = async (e) => {
     e.preventDefault()
     setButtonLoading(true)
+    setMessage(null)
     
     const data = {
-      name: e.target.username.value
+      name: e.target.username.value.trim()
     }
+
     try {
+      const result = z.object({
+        name: z.string()
+          .min(1, 'Имя не менее одного символа')
+          .max(30, 'Имя не более 30 символов')
+      }).safeParse(data)
+      
+      if(!result.success) throw result.error.issues
+
       const response = await fetch('/api/changeuser', {
         method: 'POST',
         body: JSON.stringify(data),
@@ -30,7 +39,7 @@ export default function SetUserForm() {
       setUser({...user, name: data.name})
     }
     catch (error) {
-      setMessage(error.message)
+      setMessage(error.map(e => e.message).join('<br>'))
     }
     setButtonLoading(false)
   }
@@ -51,6 +60,7 @@ export default function SetUserForm() {
         name="username"
         className={styles.input}
         defaultValue={user.name}
+        required
       />
       <button
         type="submit"
